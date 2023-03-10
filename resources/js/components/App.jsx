04523bf0160579b32c99c2ebe23b34d86d43e48e
SilvanMoura <silvancortesmoura@outlook.com>
2023-03-10 05:56:15 -0300

    Inserting the index and notFound routes

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Router from "../router/Router";
 
 const App = () => {
     return(
         <div>
-            <h1>Meu primeiro componente</h1>
+            <Router />
         </div>
     )
 }
